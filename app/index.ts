0da22276d7ff5a93022cfa82b4860ba48728e08a
galihsch <139103383+galihsch@users.noreply.github.com>
2024-04-02 14:36:25 +0000

    dsfsf

--- app/index.ts
+++ app/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import jsend from "jsend";
+import axios from "axios";
 
 import logger from "../logger";
 import errorHandler from "../middlewares/error-handler";
@@ -15,7 +16,7 @@ const isVercel = process.env.DEPLOYMENT_ENV === "vercel";
 // middleware routes
 const isProduction = process.env.NODE_ENV === "production";
 if (isProduction) {
-	app.use(helmet());
+  app.use(helmet());
 }
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,34 +24,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(jsend.middleware); // more detail on https://github.com/omniti-labs/jsend
 app.use(errorHandler);
 const options = {
-	dotfiles: "ignore",
-	etag: false,
-	extensions: ["htm", "html"],
-	index: false,
-	maxAge: "1d",
-	redirect: false,
-	setHeaders(res: express.Response) {
-		res.set("x-static-timestamp", Date.now().toString());
-	},
+  dotfiles: "ignore",
+  etag: false,
+  extensions: ["htm", "html"],
+  index: false,
+  maxAge: "1d",
+  redirect: false,
+  setHeaders(res: express.Response) {
+    res.set("x-static-timestamp", Date.now().toString());
+  },
 };
 
 app.use("/static", express.static(join(__dirname, "../public"), options));
 
 app.get("/", (_req, res: express.Response) => {
-	res.send("Open Swagger UI at http://localhost:3000/static/index.html");
+  res.send("Open Swagger UI at http://localhost:3000/static/index.html");
 });
 
 app.get("/api", (_req, res: express.Response) => {
-	res.setHeader("Content-Type", "application/json");
-	res.json({ name: "Hello world" });
+  res.setHeader("Content-Type", "application/json");
+  res.json({ name: "Hello world" });
+});
+
+app.get("/proxy", async (req, res) => {
+  try {
+    const fileUrl = req.query.fileUrl;
+    const fileName = req.query.fileName;
+
+    if (!fileUrl || !fileName) {
+      return res.status(400).send("Invalid parameters.");
+    }
+
+    const response = await axios.get(fileUrl, { responseType: "stream" });
+
+    if (!response.ok) {
+      return res
+        .status(response.status)
+        .send(`Error downloading file: ${response.statusText}`);
+    }
+
+    res.setHeader("Content-Type", response.headers["content-type"]);
+    res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`);
+
+    response.data.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error downloading file.");
+  }
 });
 
 app.use("/api/todo", todoRoutes);
 
 if (!isVercel) {
-	app.listen(3000).on("listening", () => {
-		logger.info("server is listening on port http://localhost:3000");
-	});
+  app.listen(3000).on("listening", () => {
+    logger.info("server is listening on port http://localhost:3000");
+  });
 }
 
 module.exports = app;
